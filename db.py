5330a5bf23da37ef0fe11e4df5d81b04d99c8aca
sharajmm <simplysharaj@gmail.com>
2025-08-17 17:20:32 +0530

    Update MySql connection for render

--- db.py
+++ db.py
@@ -4,12 +4,12 @@ from mysql.connector import Error
 def get_connection():
     try:
         connection = mysql.connector.connect(
-            host="mysql.railway.internal",
-            user="root",
-            password="zrZXqNinWJhpKVgJCsvCZpPDZzjmUYrh",
-            database="railway",
-            port=3306
-        )
+    host="shinkansen.proxy.rlwy.net",
+    user="root",
+    password="zrZXqNinWJhpKVgJCsvCZpPDZzjmUYrh",
+    database="railway",
+    port=20241
+)
         return connection
     except Error as e:
         print("❌ Error connecting to MySQL:", e)
